059ffdf4595b41a2bdc50e20b35337020a2e6b96
syc <syc030403@naver.com>
2023-05-02 22:43:36 +0900

    add and delete

---
+++ src/ACcounter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Countdown from './Countdown';
+
+function ACcounter() {
+  return (
+    <div>
+      <h1>Armored Core 6 Countdown</h1>
+      <Countdown />
+    </div>
+  );
+}
+
+export default ACcounter;
--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import ACcounter from './ACcounter';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <ACcounter/>
         </p>
         <a
           className="App-link"
