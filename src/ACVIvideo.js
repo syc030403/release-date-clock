9bd89166f389946d5f1ff6253cbd7f63521f3c94
syc <syc030403@naver.com>
2023-05-16 00:26:46 +0900

    add details

--- src/ACVIvideo.js
+++ src/ACVIvideo.js
@@ -1,3 +1,17 @@
 import React from 'react';
 import YouTube from 'react-youtube';
 
+function VideoPlayer() {
+    const onReady = (event) => {
+      // access to player in all event handlers via event.target
+      event.target.playVideo();
+    };
+    
+    return (
+      <div id="video-container">
+        <YouTube videoId="UJZrdD6MdAo" onReady={onReady} />
+      </div>
+    );
+  }
+  
+export default VideoPlayer
--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Countdown from './ACVIcounter';
-
+import VideoPlayer from './ACVIvideo';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          <VideoPlayer/>
+        </div>
         <p>
           <Countdown/>
         </p>
