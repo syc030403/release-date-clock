b0166cb23d23dfeaea31dfb96c6727d022312e65
syc <syc030403@naver.com>
2023-05-15 22:08:17 +0900

    retouch App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Countdown from './ACVIcounter';
-import ACVIcounter from './ACVIcounterDEMO';
+
 function App() {
   return (
     <div className="App">
